e764fb03e7b0c4c7f5905cbc126d1c72b7fa748a
Dominic Ang <zenpaiang@gmail.com>
2024-12-06 18:42:15 +0800

    rewrite redeem() + added better autocomplete functionality
    redeem() was rewritten to add a retry system for requests that failed.
    
    added a exact match filter to the autocomplete algo

--- bot.py
+++ bot.py
@@ -12,6 +12,116 @@ import io
 redeemLimits = {"inUse": False, "lastUse": 0}
 
 client = discord.Client(intents=discord.Intents.GUILDS)
+    
+async def redeem_code(session: aiohttp.ClientSession, code: str, player: dict) -> tuple[bool, str, str]:
+    timens = time.time_ns()
+    
+    login_resp = await session.post(
+        url="https://wos-giftcode-api.centurygame.com/api/player",
+        data={
+            "fid": player["id"],
+            "time": timens,
+            "sign": hashlib.md5(f"fid={player['id']}&time={timens}tB87#kPtkxqOS2".encode("utf-8")).hexdigest()
+        },
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json",
+        },
+        timeout=30
+    )
+    
+    try:
+        login_result = await login_resp.json()
+    except Exception as _:
+        return False, "error", "login error"
+    
+    if "msg" in login_result:
+        if login_result["msg"] != "success":
+            return False, "error", "login error"
+    else:
+        return True, None, "rate limited"
+    
+    timens = time.time_ns()
+    
+    redeem_resp = await session.post(
+        url="https://wos-giftcode-api.centurygame.com/api/gift_code",
+        data={
+            "cdk": code,
+            "fid": player["id"],
+            "time": timens,
+            "sign": hashlib.md5(f"cdk={code}&fid={player['id']}&time={timens}tB87#kPtkxqOS2".encode("utf-8")).hexdigest()
+        },
+        headers={
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json",
+        },
+        timeout=30
+    )
+    
+    try:
+        redeem_result = await redeem_resp.json()
+    except Exception as _:
+        return False, "error", "unknown error"
+    
+    if redeem_result["err_code"] == 40014:
+        return True, None, "gift code does not exist"
+    elif redeem_result["err_code"] == 40007:
+        return True, None, "gift code has expired"
+    elif redeem_result["err_code"] == 40008:
+        return False, "already_claimed", "already claimed"
+    elif redeem_result["err_code"] == 20000:
+        return False, "successfully_claimed", "successfully claimed"
+    else:
+        return False, "error", "unknown error"
+
+async def recursive_redeem(message: discord.Message, session: aiohttp.ClientSession, code: str, players: list, counters: dict = {"already_claimed": 0, "successfully_claimed": 0, "error": 0}, recursive_depth: int = 0): # success, counters, result            
+    results = {}
+    
+    for i in range(0, len(players), 20):
+        batch = players[i:i + 20]
+        
+        msg = "redeeming gift code" if recursive_depth == 0 else f"redeeming gift code (retry {recursive_depth})"
+        
+        await message.edit(content=f"{msg}... ({min(i, len(players))}/{len(players)}) | next update <t:{1 + int(time.time()) + (len(batch) * 3)}:R>")
+        
+        for player in batch:
+            start = time.time()
+            
+            exit, counter, result = await redeem_code(session, code, player)
+            
+            if exit:
+                await message.edit(content=f"error: {result}")
+                return
+            else:
+                counters[counter] += 1
+                results[player["name"]] = result
+                
+            await asyncio.sleep(max(0, 3 - (time.time() - start)))
+                
+    remaining_players = [player for player in players if "error" in results[player["name"]]]
+    
+    if len(remaining_players) == 0:
+        msg = (
+            f"report: gift code `{code}`\n"
+            f"successful: {counters['successfully_claimed']} | "
+            f"already claimed: {counters['already_claimed']} | "
+            f"retries: {recursive_depth}\n\n"
+            f"made with ❤️ by zenpai :D"
+        )
+        
+        await message.edit(content=msg)
+        
+        await session.close()
+        return
+                
+    await recursive_redeem(
+        message=message,
+        session=session,
+        code=code,
+        players=remaining_players,
+        counters=counters,
+        recursive_depth=recursive_depth + 1,
+    )
 
 @discord.slash_command(
     name="redeem",
@@ -33,112 +143,17 @@ async def redeem(ctx: discord.SlashContext, code: str):
     if redeemLimits["lastUse"] + 60 > time.time():
         await ctx.send("error: this command has a limit of 1 use every 1 minute to comply with WOS's rate limits.")
         return
-
+    
     with open(utils.CONFIG["playersFile"], "r") as f:
-        players = json.load(f)
+        playersObj = json.load(f)
         
-    status_message = await ctx.send(f"redeeming code... (0/{len(players.keys())} | next update <t:{int(time.time()) + 62}:R>)")
-
-    counters = {
-        "already_claimed": 0,
-        "successfully_claimed": 0,
-        "error": 0
-    }
+    players = [{"id": key, "name": playersObj[key]} for key in playersObj]
     
-    success = []
-    results = {}
-
-    batch_size = 20
-    players_list = [{"id": key, "name": players[key]} for key in players]
-    total_players = len(players_list)
-
-    async with aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl.create_default_context(cafile=certifi.where()))) as session:
-        for i in range(0, len(players_list), batch_size):
-            batch = players_list[i:i + batch_size]
-            
-            for player in batch:
-                apiStart = time.time()  
-                
-                timens = time.time_ns()
-
-                login_data = {
-                    "fid": player["id"],
-                    "time": timens,
-                    "sign": hashlib.md5(f"fid={player['id']}&time={timens}tB87#kPtkxqOS2".encode("utf-8")).hexdigest()
-                }
-                
-                async with session.post(
-                    url="https://wos-giftcode-api.centurygame.com/api/player",
-                    data=login_data,
-                    headers={
-                        "Content-Type": "application/x-www-form-urlencoded",
-                        "Accept": "application/json",
-                    },
-                    timeout=30
-                ) as login_response:
-                    login_result = await login_response.json()
-                    
-                    if "msg" in login_result:
-                        if login_result["msg"] != "success":
-                            counters["error"] += 1
-                    else:
-                        await status_message.edit(content="error: rate limited. try again in 60 seconds.")
-                        return
-                
-                timens = time.time_ns()
-                redeem_data = {
-                    "cdk": code,
-                    "fid": player["id"],
-                    "time": timens,
-                    "sign": hashlib.md5(f"cdk={code}&fid={player['id']}&time={timens}tB87#kPtkxqOS2".encode("utf-8")).hexdigest()
-                }
-                
-                async with session.post(
-                    url="https://wos-giftcode-api.centurygame.com/api/gift_code",
-                    data=redeem_data,
-                    headers={
-                        "Content-Type": "application/x-www-form-urlencoded",
-                        "Accept": "application/json",
-                    },
-                    timeout=30
-                ) as redeem_response:
-                    redeem_result = await redeem_response.json()
-                
-                    if redeem_result["err_code"] == 40014:
-                        await status_message.edit(content="error: gift code does not exist")
-                        return
-                    elif redeem_result["err_code"] == 40007:
-                        await status_message.edit(content="error: gift code has expired")
-                        return
-                    elif redeem_result["err_code"] == 40008:
-                        counters["already_claimed"] += 1
-                        results[player["name"]] = "already claimed"
-                    elif redeem_result["err_code"] == 20000:
-                        counters["successfully_claimed"] += 1
-                        results[player["name"]] = "successfully claimed"
-                        success.append(player["name"])
-                    else:
-                        counters["error"] += 1
-                        results[player["name"]] = "error"
-                
-                await asyncio.sleep(max(0, 3 - (time.time() - apiStart)))
-                
-            await status_message.edit(content=f"redeeming code... ({min(i + batch_size, total_players)}/{total_players} | next update <t:{int(time.time()) + 62}:R>)")
-
-    msg = (
-        f"report: gift code `{code}`\n"
-        f"successful: {counters['successfully_claimed']} | "
-        f"already claimed: {counters['already_claimed']} | "
-        f"errors: {counters['error']}\n\n"
-        f"made with ❤️ by zenpai :D"
-    )
+    session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=ssl.create_default_context(cafile=certifi.where())))
     
-    fakeFile = io.BytesIO("\n".join([f"{player}: {results[player]}" for player in results]).encode("utf-8"))
-
-    await status_message.edit(
-        content=msg,
-        file=discord.File(fakeFile, file_name="results.txt"),
-    )
+    message = await ctx.send("waiting...")
+    
+    await recursive_redeem(message, session, code, players)
     
     redeemLimits["inUse"] = False
     redeemLimits["lastUse"] = time.time()
@@ -148,20 +163,20 @@ async def redeem(ctx: discord.SlashContext, code: str):
     description="add a user to the redeem list",
     options=[
         discord.SlashCommandOption(
-            name="id",
-            description="the user's id",
+            name="name",
+            description="the user's name",
             required=True,
             type=discord.OptionType.STRING
         ),
         discord.SlashCommandOption(
-            name="name",
-            description="the user's name",
+            name="id",
+            description="the user's id",
             required=True,
             type=discord.OptionType.STRING
         )
     ]
 )
-async def add(ctx: discord.SlashContext, id: str, name: str):    
+async def add(ctx: discord.SlashContext, name: str, id: str):    
     if utils.intable(id):
         with open(utils.CONFIG["playersFile"], "r") as f:
             players = json.load(f)
@@ -211,9 +226,11 @@ async def user_autocomplete(ctx: discord.AutocompleteContext):
     
     with open(utils.CONFIG["playersFile"], "r") as f:
         players = json.load(f)
-    
-    if not name.startswith(f"[{utils.CONFIG['allianceName']}]"):
-        name = f"[{utils.CONFIG['allianceName']}]" + name
+        
+    for player_id, player_name in players.items():
+        if name == player_name:
+            await ctx.send(choices=[{"name": player_name, "value": player_id}])
+            return
         
     levenshteinValues = [
         (player_id, player_name, utils.levenshtein(name, player_name))
